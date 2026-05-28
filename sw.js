c573cdd944e2e9f815ca32efdd79328a5b887a49
yujun920920-del <yujun920920@gmail.com>
2026-05-28 14:08:14 +0800

    Initial commit: IELTS Practice PWA

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'ielts-v1';
+const FILES = [
+  './',
+  './index.html',
+  './reading.html',
+  './listening.html',
+  './writing.html',
+  './vocabulary.html',
+  './style.css',
+  './vocabulary.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
